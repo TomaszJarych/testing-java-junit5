83bdd18aa22285da688eef6c4f5525adddca05b1
Tomasz Jarych <tjarych@gmail.com>
2018-11-11 20:50:57 +0100

    Dependent assertions with lambdas vs bunch of independent assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.sfgpetclinic.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OwnerTest {
+
+	@Test
+	void dependetnAssertions() {
+		Owner owner = new Owner(1L, "John", "Doe");
+		owner.setCity("Dallas");
+		owner.setTelephone("123456789");
+
+		assertAll("Properties test",
+				() -> assertAll("Person properties", 
+						() -> assertEquals("John", owner.getFirstName(), "Invalid firstName"),
+						() -> assertEquals("Doe", owner.getLastName(), "Invalid lastName")),
+				() -> assertAll("Owner properties", 
+						() -> assertEquals("Dallas", owner.getCity(), "Invalid City"),
+						() -> assertEquals("123456789", owner.getTelephone(), "Invalid phone number")
+
+				));
+		// run all test despite result is wrong or right
+	}
+	
+	@Test
+	void dependentAssertionsWithoutLambdas() {
+		//given
+		Owner owner = new Owner(1L, "John", "Doe");
+		owner.setCity("Dallas");
+		owner.setTelephone("123456789");
+		
+		//then
+		assertEquals("John", owner.getFirstName(), "Invalid firstName");
+		assertEquals("Doe", owner.getLastName(), "Invalid lastName");
+		assertEquals("Dallas", owner.getCity(), "Invalid City");
+		assertEquals("123456789", owner.getTelephone(), "Invalid phone number");
+		
+		// ends after first fail;
+		
+		
+	}
+
+}
