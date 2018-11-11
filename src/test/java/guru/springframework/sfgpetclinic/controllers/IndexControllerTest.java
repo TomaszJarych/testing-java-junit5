2fa0bb688126f4b669c9d0c5bb8f7c7d37e00c3d
Tomasz Jarych <tjarych@gmail.com>
2018-11-11 22:22:27 +0100

    Timeout test

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,9 +1,11 @@
 package guru.springframework.sfgpetclinic.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,17 +24,35 @@ class IndexControllerTest {
 		assertEquals("index", indexController.index(), "index() method failed");
 	}
 
-	@DisplayName(value="Test exception")
+	@DisplayName(value = "Test exception")
 	@Test
 	void testOupsHandler() {
-		assertThrows(ValueNotFoundException.class, 
-				() -> {
-					indexController.oupsHandler();	
-				});
-//		assertEquals("notimplemented", indexController.oupsHandler(), " oupsHandler() failded");
-//		assertTrue("notimplemented".equals(indexController.oupsHandler()), () -> "This is some expensive "
-//				+ "Messeage to build " + "for my tets. Make if you only really, really  have to");
+		assertThrows(ValueNotFoundException.class, () -> {
+			indexController.oupsHandler();
+		});
+		// assertEquals("notimplemented", indexController.oupsHandler(), "
+		// oupsHandler() failded");
+		// assertTrue("notimplemented".equals(indexController.oupsHandler()), ()
+		// -> "This is some expensive "
+		// + "Messeage to build " + "for my tets. Make if you only really,
+		// really have to");
 		// lambda in JUnit 5
+	}
+
+	@Disabled("Demo of timeout test")
+	@Test
+	void testTimeOut() {
+		assertTimeout(Duration.ofMillis(100), () -> Thread.sleep(3000));
+		
+		//single thread - execution and assertion, one after one
+	}
+	
+	@Disabled("Demo of timeout test")
+	@Test
+	void testTimeOutPreempt() {
+		assertTimeoutPreemptively(Duration.ofMillis(100), () -> Thread.sleep(3000));
+		
+		//separate threads - test and assertion
 	}
 
 }
