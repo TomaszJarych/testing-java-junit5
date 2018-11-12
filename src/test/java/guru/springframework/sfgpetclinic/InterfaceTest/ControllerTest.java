ccf3fb836f1f239e19455c1047165a31d801d690
Tomasz Jarych <tjarych@gmail.com>
2018-11-12 17:59:10 +0100

    using default method in test interface

--- src/test/java/guru/springframework/sfgpetclinic/InterfaceTest/ControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/InterfaceTest/ControllerTest.java
@@ -1,13 +1,22 @@
 package guru.springframework.sfgpetclinic.InterfaceTest;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+@TestInstance(Lifecycle.PER_CLASS)
 @Tag("controller")
 public interface ControllerTest {
-
-	
 	// different way to tag test classes, without annotation
 	// instead using the tagged Interface
 	// Need to configure run as in IDE
 	// new feature in JUnit 5!
+	
+	@BeforeAll
+	default void beforeAll() {
+		System.out.println("Lets do something here! -----------------");
+		// @TestInstance(Lifecycle.PER_CLASS) needed to work
+	}
+	
 }
