f619ecb4fc26292191a703436d7f006916243a18
Tomasz Jarych <tjarych@gmail.com>
2018-11-12 17:20:47 +0100

    Test tags in IDE

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,10 +2,14 @@ package guru.springframework.sfgpetclinic.model;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Tag;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.Test;
 
+@Tag("model")
 class OwnerTest {
 
 	@Test
--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -2,8 +2,9 @@ package guru.springframework.sfgpetclinic.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
+@Tag("model")
 class PersonTest {
 
 	@Test
@@ -12,8 +13,10 @@ class PersonTest {
 		Person person = new Person(1L, "John", "Doe");
 		// then
 		assertAll("Test set, next will be lambdas",
-				() -> assertEquals("John", person.getFirstName(), "Wrong firstName"),
-				() -> assertEquals("Doe", person.getLastName(), "Wrong lastName"));
+				() -> assertEquals("John", person.getFirstName(),
+						"Wrong firstName"),
+				() -> assertEquals("Doe", person.getLastName(),
+						"Wrong lastName"));
 
 	}
 
