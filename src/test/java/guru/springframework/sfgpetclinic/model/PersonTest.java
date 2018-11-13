35087210624e0cd53456099f1b1baad6e45c9c75
Tomasz Jarych <tjarych@gmail.com>
2018-11-13 12:21:53 +0100

    JUnit Test Dependency Injection

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,11 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.RepeatedTest.LONG_DISPLAY_NAME;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 
 @Tag("model")
 class PersonTest {
@@ -25,8 +29,14 @@ class PersonTest {
 	
 	@RepeatedTest(value=10, name="{displayName} : {currentRepetition} of {totalRepetitions}")
 	@DisplayName("My Repeated Test")
-	@Test
 	void repeatedTest() {
 			//TODO
+	}
+	
+	@RepeatedTest(value=10, name=LONG_DISPLAY_NAME)
+	@DisplayName("Repeated test with DI")
+	void repeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo, TestReporter testReporter) {
+		System.out.println(testInfo.getDisplayName()+" : "+repetitionInfo.getCurrentRepetition());
+		
 	}
 }
