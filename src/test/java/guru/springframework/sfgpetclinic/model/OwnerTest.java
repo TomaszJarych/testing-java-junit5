8d60b0c6ae0c79a96ef86a6b23676d996f834ed4
Tomasz Jarych <tjarych@gmail.com>
2018-11-14 15:00:39 +0100

    JUnit Parameterized Tests - ENUM Source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -2,8 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.ParameterizedTest.DEFAULT_DISPLAY_NAME;
 import static org.junit.jupiter.params.ParameterizedTest.DISPLAY_NAME_PLACEHOLDER;
 
@@ -11,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @Tag("model")
@@ -23,46 +23,56 @@ class OwnerTest {
 		owner.setTelephone("123456789");
 
 		assertAll("Properties test",
-				() -> assertAll("Person properties", 
-						() -> assertEquals("John", owner.getFirstName(), "Invalid firstName"),
-						() -> assertEquals("Doe", owner.getLastName(), "Invalid lastName")),
-				() -> assertAll("Owner properties", 
-						() -> assertEquals("Dallas", owner.getCity(), "Invalid City"),
-						() -> assertEquals("123456789", owner.getTelephone(), "Invalid phone number")
+				() -> assertAll("Person properties",
+						() -> assertEquals("John", owner.getFirstName(),
+								"Invalid firstName"),
+						() -> assertEquals("Doe", owner.getLastName(),
+								"Invalid lastName")),
+				() -> assertAll("Owner properties",
+						() -> assertEquals("Dallas", owner.getCity(),
+								"Invalid City"),
+						() -> assertEquals("123456789", owner.getTelephone(),
+								"Invalid phone number")
 
 				));
 		// run all test despite result is wrong or right
 	}
-	
+
 	@Test
 	void dependentAssertionsWithoutLambdas() {
-		//given
+		// given
 		Owner owner = new Owner(1L, "John", "Doe");
 		owner.setCity("Dallas");
 		owner.setTelephone("123456789");
-		
-		//then
+
+		// then
 		assertEquals("John", owner.getFirstName(), "Invalid firstName");
 		assertEquals("Doe", owner.getLastName(), "Invalid lastName");
 		assertEquals("Dallas", owner.getCity(), "Invalid City");
 		assertEquals("123456789", owner.getTelephone(), "Invalid phone number");
-		
+
 		// ends after first fail;
 		/*
 		 * hamcrest example
 		 */
 		assertThat(owner.getCity(), is("Dallas"));
 	}
-	
+
 	@DisplayName("Value source Test -")
-	@ParameterizedTest(name=DISPLAY_NAME_PLACEHOLDER+" "+DEFAULT_DISPLAY_NAME)
-	@ValueSource(strings= {"Spring","Framework","Guru"})
-	void parameterizedTest(String val){
+	@ParameterizedTest(name = DISPLAY_NAME_PLACEHOLDER + " "
+			+ DEFAULT_DISPLAY_NAME)
+	@ValueSource(strings = {"Spring", "Framework", "Guru"})
+	void parameterizedTest(String val) {
 		System.out.println(val);
 	}
-	
-	
-	
-	
+
+	@DisplayName("Enum source Test -")
+	@ParameterizedTest(name = DISPLAY_NAME_PLACEHOLDER + " "
+			+ DEFAULT_DISPLAY_NAME)
+	@EnumSource(OwnerType.class)
+	void enumTest(OwnerType ownerType) {
+		System.out.println(ownerType);
+
+	}
 
 }
