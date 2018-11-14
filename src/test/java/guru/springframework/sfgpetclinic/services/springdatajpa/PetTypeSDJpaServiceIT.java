f48185965cd4c6fe1c31ff3cf2621df040cb5f1d
Tomasz Jarych <tjarych@gmail.com>
2018-11-14 17:09:10 +0100

    Unit Tests vs Integration Tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -0,0 +1,36 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PetTypeSDJpaServiceIT {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testPetTypeSDJpaService() {
+	}
+
+	@Test
+	void testFindAll() {
+	}
+
+	@Test
+	void testFindById() {
+	}
+
+	@Test
+	void testSave() {
+	}
+
+	@Test
+	void testDelete() {
+	}
+
+	@Test
+	void testDeleteById() {
+	}
+
+}
